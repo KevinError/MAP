af45fa5b45da390278ac74f9ff295d35b310c169
Krystal Leon <krystalleon10@gmail.com>
2019-02-02 19:10:50 -0800

    test website deploy

---
+++ MyAcademicPyramid/DataAccessLayer/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DataAccessLayer.Controllers
+{
+    public static class UserController
+    {
+        public static IEnumerable<User> Get()
+        {
+            MyAcademicPyramidEntities entities = new MyAcademicPyramidEntities();
+            return entities.Users.ToList();
+        }
+
+        public static User Get(string email)
+        {
+            MyAcademicPyramidEntities entities = new MyAcademicPyramidEntities();
+            return entities.Users.FirstOrDefault(u => u.Email.Equals(email));
+        }
+    }
+}
---
+++ MyAcademicPyramid/DemoProject/ProgramDAL.cs
@@ -0,0 +1,37 @@
+﻿using DataAccessLayer;
+using DataAccessLayer.Controllers;
+using System;
+using System.Collections.Generic;
+
+namespace DemoProject
+{
+    class ProgramDAL
+    {
+
+        static void Main(string[] args)
+        {
+            // Get all users.
+            Console.WriteLine("Get all users");
+            IEnumerable<User> users = UserController.Get();
+
+            // Output all userss
+            Console.WriteLine("Output all users:");
+            foreach(User u in users)
+            {
+                // User full name and email
+                Console.WriteLine("\n" + u.FirstName + " " + u.MiddleName + " " + u.LastName + ", " + u.Email);
+
+                // User claims
+                foreach(Claim c in u.Claims)
+                {
+                    Console.Write(c.ID + ": " + c.Value + ", ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("End");
+            Console.ReadKey(true);
+        }
+
+        
+    }
+}
